bb73b907aa3db1d100f75bef170804a3c38fa1b2
Chris Bargren <cbargren@gmail.com>
2022-10-18 16:21:07 -0700

    One more time

--- libs/internal-plugin/src/executors/heroku-deploy/executor.ts
+++ libs/internal-plugin/src/executors/heroku-deploy/executor.ts
@@ -3,7 +3,7 @@ import { HerokuDeployExecutorSchema } from './schema';
 
 export default async function runExecutor(schema: HerokuDeployExecutorSchema) {
   const cwd = schema.distLocation;
-  execSync(`heroku container:login`, { cwd });
+  execSync(`heroku container:login`, { cwd, stdio: 'inherit' });
   execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd });
   execSync(`heroku container:release web --app ${schema.herokuAppName}`, {
     cwd,
